8ff0dc7f5a563d7a00a4d708e2eda5fb36c3bc4a
gitpBansal031 <bansalpulkit3103@gmail.com>
2024-06-30 23:42:01 +0530

    style routes

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-const defaultLink="https://business.booking.com/storage/assets/media/34/homepage-booking-us-v1_ac2bada813f142765fe9b4f3fbe3e3da59f0864429.webp";
+const defaultLink="https://www.bsr.org/images/heroes/bsr-travel-hero..jpg";
 const listingSchema=new mongoose.Schema({
     title:{
         type:String,
@@ -8,7 +8,7 @@ const listingSchema=new mongoose.Schema({
     description:{
         type:String
     },
-    iddmage:{
+    image:{
         type:String,
         default: defaultLink,
         set: (v)=> v===""?defaultLink:v
